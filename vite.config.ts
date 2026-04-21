311a3b87ab5517d56029716112ca410529f65323
Felix Ranesberger <felix.ranesberger@in2code.de>
2026-04-21 08:12:52 +0200

    fix(feat): disable lightningcss light-dark transpilation, because it breaks darkmode of styleguide

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+import { Features } from 'lightningcss'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -23,6 +24,14 @@ export default defineConfig({
       },
     },
     manifest: true,
+  },
+  css: {
+    lightningcss: {
+      // light-dark transpilation breaks at the moment and this disables it
+      // https://github.com/parcel-bundler/lightningcss/issues?q=is:issue%20state:open%20light-dark
+      // TODO: check if this is fixed in lightningcss:
+      exclude: Features.LightDark,
+    },
   },
   worker: {
     format: 'es',
